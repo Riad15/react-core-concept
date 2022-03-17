6694d7fe00aa2a918acc862e3fc0ddfb3dac55f8
asif riad <asifriad15@gmail.com>
2022-03-17 17:58:03 +0600

    for more practice

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Country = (props) => {
-    const { area, population, name } = props.country;
+    const { name, area, population } = props.country;
     return (
         <div className='country'>
-            <h1>Country Name: {name.common} </h1>
-            <p>Population: {population}</p>
-            <p>Area: {area} </p>
+            <h1>Name: {name.common} </h1>
+            <p>Population: {population} </p>
+            <p>Ariea: {area} </p>
         </div>
+
     );
 };
 
